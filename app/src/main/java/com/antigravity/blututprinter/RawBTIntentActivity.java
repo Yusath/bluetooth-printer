c144633c338aa42aa1c1d81d84efe30ebc31b417
yusathid <yusrilathallah@gmail.com>
2026-05-31 10:42:22 +0700

    -

--- app/src/main/java/com/antigravity/blututprinter/RawBTIntentActivity.java
+++ app/src/main/java/com/antigravity/blututprinter/RawBTIntentActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Base64;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
